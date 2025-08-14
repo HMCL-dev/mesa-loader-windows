ca9da75202efea18a18af5faac080a11304a6490
Glavo <zjx001202@gmail.com>
2025-08-15 01:10:53 +0800

    Migrating from OSSRH to Central Portal

--- build.gradle.kts
+++ build.gradle.kts
@@ -179,7 +179,9 @@ signing {
 nexusPublishing {
     repositories {
         sonatype {
-            stagingProfileId.set(rootProject.ext["sonatypeStagingProfileId"].toString())
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
+
             username.set(rootProject.ext["sonatypeUsername"].toString())
             password.set(rootProject.ext["sonatypePassword"].toString())
         }
